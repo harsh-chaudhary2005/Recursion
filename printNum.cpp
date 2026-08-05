46aab976ef36630f445328b22c82c50798f5ac61
Harsh Chaudhary <hc492250@gmail.com>
2026-08-05 11:37:53 +0530

    Add files via upload

---
+++ printNum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printNum(int n){
+    if (n == 1)
+    {
+        cout << 1;
+        return;
+    }
+    cout << n << " ";
+    printNum(n-1);
+}
+int main(){
+  int n = 1545;
+  printNum(n);
+  cout << endl;
+  return 0;
+}
