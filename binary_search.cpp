7ad7bed4590a2b50ae2f1ede5566e36015b30d0d
Harsh Chaudhary <hc492250@gmail.com>
2026-08-06 06:47:34 +0530

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+class Solution {
+public:
+    int binSearch(vector<int>& arr, int tar, int st, int end){
+        if(st <= end){
+            int mid = (st + end)/2;
+            if(arr[mid] == tar){
+                return mid;
+            } else if(arr[mid] <= tar){
+                return binSearch(arr, tar, mid+1, end);
+            } else{
+                return binSearch(arr, tar, st, mid-1);
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& arr, int tar) {
+        return binSearch(arr, tar, 0, arr.size()-1);
+
+    }
+};
---
+++ print_all_subsets.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printSearch(vector<int> &arr, vector<int> &ans, int i){
+    if (i == arr.size())
+    {
+        for(int val : ans){
+            cout << val << " ";
+        }
+        cout << endl;
+        return;
+    }
+    //include
+    ans.push_back(arr[i]);
+    printSearch(arr, ans, i+1);
+    //exclude
+    ans.pop_back();   //backtracking 
+    printSearch(arr, ans, i+1);
+}
+int main(){
+  vector<int> arr = {1, 2, 3};
+  vector<int> ans;
+
+  printSearch(arr, ans, 0);
+  
+
+  return 0;
+}
